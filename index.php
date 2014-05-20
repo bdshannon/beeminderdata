1b3b72c360952aa447470f9cb3d1086d12433878
Brian Shannon <bdshannon@users.noreply.github.com>
2014-05-20 17:07:39 +0200

    TableBuilder class to get rid of some tedium

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta content="text/html; charset=utf-8" />
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
     <!-- Optional theme -->
@@ -29,8 +30,52 @@
 <body>
     <p>I have stuff to do. These are some of those things.</p>
     <?php
+        class TableBuilder {
+            function __construct() {
+                $args = func_get_args();
+                if (count($args) < 2) {
+                    throw new BadMethodCallException("Less than two arguments.");
+                }
+                $this->headers = array_shift($args);
+                $this->data = $args;
+                $this->css_classes = [];
+                $this->value_if_empty = "";
+                return $this;
+            }
+
+            private function toHtmlTableHeader() {
+                $html = "<thead><tr>";
+                foreach($this->headers as $header) {
+                    $html .= sprintf("<th>%s</th>", ucwords($header));
+                }
+                $html .= "</tr></thead>";
+                return $html;
+            }
+
+            private function toHtmlTableBody() {
+                $html = "<tbody>";
+                foreach($this->data as $dataobj) {
+                    $html .= "<tr>";
+                    foreach($this->headers as $header) {
+                        $html .= sprintf("<td>%s</td>", $dataobj->{$header});
+                    }
+                    $html .= "</tr>";
+                }
+                $html .= "</tbody>";
+                return $html;
+            }
+
+            public function toHtml() {
+                $html = sprintf("<table class=\"%s\">", join($this->css_classes));
+                $html .= $this->toHtmlTableHeader();
+                $html .= $this->toHtmlTableBody();
+                $html .= "</table>";
+                return $html;
+            }
+        }
+
         # FIXME: fetching from a url should be done in a cronjob
-        $urlfmt = "https://www.beeminder.com/api/v1/users/%s/goals/%s.json?datapoints=True&auth_token=%s";
+        $urlfmt = "https://www.beeminder.com/api/v1/users/%s/goals/%s.json?datapoints=False&auth_token=%s";
         $tokenfile = "./token.txt"; # FIXME: this should definitely be changed
         $token = trim(file_get_contents($tokenfile));
 
@@ -41,31 +86,20 @@
         $goalslugsstring = trim(file_get_contents($goalslugsfile));
         $goalslugs = split("\n", $goalslugsstring);
 
-        $goalmessage = function($lane) {
-            # FIXME: Works only if positive lane is good
-            $goallanemessages = [
-                2 => "I'm exceeding expectations!",
-                1 => "I'm on track.",
-                -1 => "I'm nearly off track! Annoy me!",
-                -2 => "I'm off track. Poke me until I tow the line",
-            ];
-
-            $lane = $lane > 2 ? 2 : $lane;
-            $lane = $lane < -2 ? -2 : $lane;
-            return $goallanemessages[$lane]; 
-        };
-
-        echo "<table>";
+        $goals = [];
         foreach($goalslugs as $goalslug) {
             $url = sprintf($urlfmt, $username, trim($goalslug), $token);
             $goaljsonstring = file_get_contents($url);
-            $goaljson = json_decode($goaljsonstring);
-
-            echo "<tr>";
-            echo sprintf("<td>%s</td><td>%s</td>", $goaljson->title, $goalmessage($goaljson->lane));
-            echo "</tr>";
+            $goal = json_decode($goaljsonstring);
+            $goals[] = $goal;
         }
-        echo "</table>";
+
+        $table_args = $goals;
+        array_unshift($table_args, ["title", "pledge"]);
+        $tbuilderfactory = new ReflectionClass("TableBuilder");
+        $tbuilder = $tbuilderfactory->newInstanceArgs($table_args);
+        $tbuilder->css_classes[] = "table";
+        echo $tbuilder->toHtml();
     ?>
 </body>
 </html>
