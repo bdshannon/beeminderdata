d7bb152ef0a0177e01789a9df571f8f362b6edbc
Brian Shannon <bdshannon@users.noreply.github.com>
2014-05-23 12:50:31 +0200

    Add my name as copyright owner

--- htdocs/index.php
+++ htdocs/index.php
@@ -14,6 +14,8 @@
 
     You should have received a copy of the GNU Affero General Public License
     along with BeeminderPublicData.  If not, see <http://www.gnu.org/licenses/>.
+
+    Copyright Brian Shannon 2014
 **/
 ?>
 <!DOCTYPE html>
